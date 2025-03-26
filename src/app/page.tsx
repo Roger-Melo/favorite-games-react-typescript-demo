94df99f79183564621b240fe738bae9d22bfa0b6
Roger-Melo <rogerw592@gmail.com>
2025-03-26 10:44:12 -0300

    game card styled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,35 @@
+import { Button } from "@/components/ui/button"
+import { Card, CardDescription, CardFooter, CardTitle, } from "@/components/ui/card"
+import { Trophy, X } from "lucide-react"
+
+function GameCard() {
+  return (
+    <Card className="p-4">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-4">
+          <Trophy size={26} />
+          <div>
+            <CardTitle className="text-lg">Elden Ring</CardTitle>
+            <CardDescription>From Software</CardDescription>
+          </div>
+        </div>
+        <CardFooter className="p-0">
+          <Button variant="ghost" className="cursor-pointer">
+            <X />
+          </Button>
+        </CardFooter>
+      </div>
+    </Card>
+
+  )
+}
+
 function Home() {
-  return <h2>Hello</h2>
+  return (
+    <div className="max-w-[420px] px-4 py-20 mx-auto">
+      <GameCard />
+    </div>
+  )
 }
 
 export default Home
