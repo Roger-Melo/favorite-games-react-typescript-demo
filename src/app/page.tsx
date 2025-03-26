0eca81bdcdf23095078fa868c74f01ae56a03cfc
Roger-Melo <rogerw592@gmail.com>
2025-03-26 13:19:16 -0300

    GameCard props typed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,20 @@ import { Button } from "@/components/ui/button"
 import { Card, CardDescription, CardFooter, CardTitle, } from "@/components/ui/card"
 import { Trophy, X } from "lucide-react"
 
-function GameCard() {
+type GameCardProps = {
+  title: string
+  developer: string
+}
+
+function GameCard({ title, developer }: GameCardProps) {
   return (
     <Card className="p-4">
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-4">
-          <Trophy size={26} />
+          <Trophy size={26} strokeWidth={1} />
           <div>
-            <CardTitle className="text-lg">Elden Ring</CardTitle>
-            <CardDescription>From Software</CardDescription>
+            <CardTitle className="text-lg">{title}</CardTitle>
+            <CardDescription>{developer}</CardDescription>
           </div>
         </div>
         <CardFooter className="p-0">
@@ -20,14 +25,13 @@ function GameCard() {
         </CardFooter>
       </div>
     </Card>
-
   )
 }
 
 function Home() {
   return (
     <div className="max-w-[420px] px-4 py-20 mx-auto">
-      <GameCard />
+      <GameCard title="Elden Ring" developer="From Software" />
     </div>
   )
 }
