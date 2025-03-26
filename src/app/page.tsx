ca9f6a794a9de0c01f7f07d1a7915f294f0e2c24
Roger-Melo <rogerw592@gmail.com>
2025-03-26 13:42:36 -0300

    create Header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button"
 import { Card, CardDescription, CardFooter, CardTitle, } from "@/components/ui/card"
-import { Trophy, X } from "lucide-react"
+import { Trophy, X, Gamepad2 } from "lucide-react"
 
 type GameCardProps = {
   title: string
@@ -28,9 +28,19 @@ function GameCard({ title, developer }: GameCardProps) {
   )
 }
 
+function Header() {
+  return (
+    <header className="flex flex-col items-center">
+      <Gamepad2 size={76} />
+      <h1 className="text-4xl font-medium tracking-tight">Seus Games Favoritos</h1>
+    </header>
+  )
+}
+
 function Home() {
   return (
-    <div className="max-w-[420px] px-4 py-20 mx-auto">
+    <div className="max-w-[420px] px-4 py-20 mx-auto flex flex-col gap-6">
+      <Header />
       <GameCard title="Elden Ring" developer="From Software" />
     </div>
   )
