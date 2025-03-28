d6b32e81fada90989264855d0c77638ba870a772
Roger-Melo <rogerw592@gmail.com>
2025-03-28 13:08:12 -0300

    e2e: create createGame function

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,8 +1,14 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, type Page } from "@playwright/test"
 
-test.beforeEach(async ({ page }) => {
-  await page.goto("/")
-})
+type Game = {
+  title: string
+  studio: string
+}
+
+type CreateGameProps = {
+  game: Game,
+  page: Page,
+}
 
 const favGames = [
   { title: "BloodBorne", studio: "From Software" },
@@ -10,32 +16,36 @@ const favGames = [
   { title: "Fatal Frame II", studio: "Tecmo Co." },
 ] as const
 
+async function createGame({ game, page }: CreateGameProps) {
+  const inputGame = page.getByRole("textbox", { name: "Game" })
+  const inputStudio = page.getByRole("textbox", { name: "Studio" })
+  const buttonAddGame = page.getByRole("button", { name: "Adicionar Game" })
+  await inputGame.click()
+  await inputGame.fill(game.title)
+  await inputStudio.click()
+  await inputStudio.fill(game.studio)
+  await buttonAddGame.click()
+}
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("/")
+})
+
 test.describe("New Fav Game", () => {
   test("should allow to add game items", async ({ page }) => {
-    const inputGame = page.getByRole("textbox", { name: "Game" })
-    const inputStudio = page.getByRole("textbox", { name: "Studio" })
-    const buttonAddGame = page.getByRole("button", { name: "Adicionar Game" })
     const listItem = page.locator("ul > li")
 
     // Create 1st game
-    await inputGame.click()
-    await inputGame.fill(favGames[0].title)
-    await inputStudio.click()
-    await inputStudio.fill(favGames[0].studio)
-    await buttonAddGame.click()
+    await createGame({ game: favGames[0], page })
 
     // Make sure the list only has one game item.
     await expect(listItem).toContainText([favGames[0].title])
     await expect(listItem).toContainText([favGames[0].studio])
 
     // Create 2st game
-    await inputGame.click()
-    await inputGame.fill(favGames[1].title)
-    await inputStudio.click()
-    await inputStudio.fill(favGames[1].studio)
-    await buttonAddGame.click()
+    await createGame({ game: favGames[1], page })
 
-    // Make sure the list now has two game items.
+    // Make sure the list now has two game items
     await expect(listItem).toContainText([favGames[0].title, favGames[1].title])
     await expect(listItem).toContainText([favGames[0].studio, favGames[1].studio])
   })
